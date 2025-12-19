5e548d56a74e891547d173e5ca4d2a7ee87fe471
Luiz Gustavo <l19051997@gmail.com>
2025-12-19 20:01:24 -0300

    refactor: format tsconfig.json for improved readability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-    }
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Google Sans Code",
+      cssVariable: "--font-google-sans-code",
+      weights: ["400", "700"],
+      subsets: ["latin"],
+      fallbacks: ["sans-serif"],
+    }]
+  },
+
+  integrations: [icon()],
 });
