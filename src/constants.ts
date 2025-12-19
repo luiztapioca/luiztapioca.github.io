e1b7a76f6de033470d519579d171a0398ae9d694
Luiz Gustavo <l19051997@gmail.com>
2025-12-19 20:01:33 -0300

    feat: define site and user configuration constants with social links

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,17 @@
-export enum Config {
-    SiteTitle = "Portfolio",
-    SiteDescription = "Junior backend developer.",
-    UserName = "Luiz Gustavo",
+import type { Social } from "./types";
+
+export const SiteConfig = {
+    siteTitle: "NAME",
+    siteDescription: "DESCRIPTION",
+}
+
+export const UserConfig = {
+    name: "USER NAME",
+    avatar: "AVATAR URL",
+    bio: "BIO",
+    socials: [
+        { link: "https://github.com/USERNAME", icon: "mdi:github" },
+        { link: "https://linkedin.com/in/USERNAME", icon: "mdi:linkedin" },
+        { link: "https://mastodon.social/@USERNAME", icon: "mdi:mastodon" }
+    ] as Social[],
 }
