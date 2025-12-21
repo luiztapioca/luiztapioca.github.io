d151b324780ec3d2f7e3477a64bbc223543b7a4d
Luiz Gustavo <l19051997@gmail.com>
2025-12-20 21:57:56 -0300

    style: refactor layout and styling of ExperiencesCard component for improved presentation

--- src/components/react/ExperiencesCard.jsx
+++ src/components/react/ExperiencesCard.jsx
@@ -4,15 +4,15 @@ function Experiences({ experiences }) {
     const [experience, setExperience] = useState(null);
 
     return (
-        <div className="flex flex-row items-start">
-            <div className="h-84 overflow-y-auto bg-slate-950 rounded-l-lg">
+        <div className="flex flex-col md:flex-row items-start">
+            <div className="md:h-84 overflow-y-auto w-full md:w-auto">
                 <ul className={``}>
                 {experiences.map((exp) => {
                     const { company } = exp.data;
                     return (
                         <li
                             key={exp.id}
-                            className={`p-4 text-xs md:text-lg bg-slate-950 ease-in-out duration-200 ${exp.id == experience?.id ? 'font-bold border-l-4 border-rose-400' : ''}`}
+                            className={`p-4 text-xs md:text-lg ease-in-out duration-200 ${exp.id == experience?.id ? 'font-bold border-l-4 bg-slate-950 border-rose-400' : ''}`}
                             onClick={ () => setExperience(exp) }
                         >
                             { company }
@@ -21,7 +21,7 @@ function Experiences({ experiences }) {
                 })}
                 </ul>
             </div>
-            <div className="bg-slate-950 p-4 h-84 overflow-y-auto flex-1 rounded-r-lg">
+            <div className="p-4 flex-1">
                 { experience && (
                     <div>
                         <h2 className="font-bold mb-2 text-xs md:text-lg">
@@ -31,7 +31,7 @@ function Experiences({ experiences }) {
                         {experience.data.bullets?.map((bullet, index) => {
                             return (
                                 <li 
-                                    className="mb-2 text-xs md:text-lg list-disc list-inside"
+                                    className="mb-2 text-xs md:text-lg list-inside before:content-['★'] before:text-rose-400 before:mr-2"
                                     key={index}
                                 >
                                     { bullet }
