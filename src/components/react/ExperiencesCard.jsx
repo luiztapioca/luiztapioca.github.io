61aee4b741f530009627eb7979795f05271cb135
Luiz Gustavo <l19051997@gmail.com>
2025-12-24 14:17:29 -0300

    style: update ExperiencesCard component to display company name and enhance layout with icons

--- src/components/react/ExperiencesCard.jsx
+++ src/components/react/ExperiencesCard.jsx
@@ -24,10 +24,17 @@ function Experiences({ experiences }) {
             <div className="p-4 flex-1">
                 { experience && (
                     <div>
-                        <h2 className="font-bold mb-2 text-xs md:text-lg">
-                            { experience.data.position } <span className="text-rose-400">@</span> { experience.data.description }
+                        <h2 className="font-bold mb-1 text-xs md:text-lg">
+                            { experience.data.position } <span className="text-rose-400">@</span> { experience.data.company }
                         </h2>
-                        <p className="italic mb-4 text-xs md:text-lg">
+                        <h3
+                            className="italic text-xs md:text-lg mb-2 flex items-center gap-2"
+                        >
+                            <svg className="h-4 w-4 md:h-5 md:w-5 fill-rose-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>account-box</title><path d="M6,17C6,15 10,13.9 12,13.9C14,13.9 18,15 18,17V18H6M15,9A3,3 0 0,1 12,12A3,3 0 0,1 9,9A3,3 0 0,1 12,6A3,3 0 0,1 15,9M3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3H5C3.89,3 3,3.9 3,5Z" /></svg>
+                            { experience.data.description }
+                        </h3>
+                        <p className="italic mb-4 text-xs md:text-lg flex items-center gap-2">
+                            <svg className="h-4 w-4 md:h-5 md:w-5 fill-rose-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>calendar</title><path d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" /></svg>
                             { new Date(experience.data.startDate).toLocaleDateString('en-US', { year: 'numeric', month: 'numeric' }) } - { experience.data.endDate ? new Date(experience.data.endDate).toLocaleDateString('en-US', { year: 'numeric', month: 'numeric' }) : 'Presente' }
                         </p>
                         <ul>
