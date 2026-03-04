d7387e63cb636248f07e42e8a5de9a6c77dd8aa9
Luiz Gustavo <l19051997@gmail.com>
2026-03-04 20:18:03 -0300

    responsive

--- src/components/svelte/theme-toggle.svelte
+++ src/components/svelte/theme-toggle.svelte
@@ -3,10 +3,10 @@
     import { SunMoon } from '@lucide/svelte';
 </script>
 
-<button onclick={ toggleMode }>
+<button onclick={ toggleMode } class="p-1 hover:opacity-80 transition-opacity">
     <SunMoon
       class="
-        h-[1.2rem] w-[1.2rem]
+        h-5 w-5 sm:h-[1.2rem] sm:w-[1.2rem]
         text-[#657b83] dark:text-[#839496]
         hover:text-[#268bd2] dark:hover:text-[#268bd2]
         ml-2 sm:ml-0 mt-2"/>
