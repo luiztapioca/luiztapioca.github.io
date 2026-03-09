3b447fab9d8b3042de5932f2ba65815f4effa9ab
Luiz Gustavo <l19051997@gmail.com>
2026-03-09 18:36:10 -0300

    mudanças no tema

--- src/components/svelte/theme-toggle.svelte
+++ src/components/svelte/theme-toggle.svelte
@@ -6,6 +6,7 @@
 <button
   onclick={ toggleMode }
   class="
+    group relative
     flex-shrink-0 flex items-center justify-center
     text-[#657b83] dark:text-[#839496]
     hover:text-[#d33682] dark:hover:text-[#d33682]
@@ -16,4 +17,12 @@
     [-webkit-tap-highlight-color:transparent]
   ">
     <SunMoon class="h-3.5 w-3.5 sm:h-[1.1rem] sm:w-[1.1rem]" />
+    <span class="
+        absolute -bottom-5 left-1/2 -translate-x-1/2
+        text-[0.6rem] whitespace-nowrap
+        text-[#93a1a1] dark:text-[#586e75]
+        opacity-0 group-hover:opacity-100
+        transition-opacity duration-200
+        pointer-events-none select-none
+    ">(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧</span>
 </button>
